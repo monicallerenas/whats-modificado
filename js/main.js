c6b7c9d44dfbbb0e8a7bb6affe8e5cb34060dc39
monicallerenas <kmonik28@gmail.com>
2016-06-10 15:16:08 -0500

    solucionado un error

--- js/main.js
+++ js/main.js
@@ -7,6 +7,15 @@ function pintarMensaje(texto) {
    var nombreUsuario = 'Monica'; 
    var ella = '<div class="escribe"><p class="morado">' + nombreUsuario + '</p><p>' + texto + '</p><p class="hora-left">' + fecha + '</p></div>';
    var elementoDom = $(ella);
-   $('#mensajes').append(elemetoDom);
+   $('#mensajes').append(elementoDom);
    
 };
+
+$('#btnEnviar').click(function(){
+   
+   var texto = $('#inputMensaje').val();
+   console.log(texto);
+   pintarMensaje(texto);
+   $('#inputMensaje').val('');
+
+});
